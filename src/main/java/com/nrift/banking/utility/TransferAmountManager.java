3884b2e08e08db96666de8a66cc2ed6bc3b65d38
arnaba <arnaba@ARNABA.user.nrifintech.com>
2014-09-09 10:05:23 +0530

    Changes in TransferAmountManager

--- src/main/java/com/nrift/banking/utility/TransferAmountManager.java
+++ src/main/java/com/nrift/banking/utility/TransferAmountManager.java
@@ -1,37 +1,3 @@
-<<<<<<< Upstream, based on master
-package com.nrift.banking.utility;
-
-import java.sql.Connection;
-import java.sql.Timestamp;
-
-import javax.servlet.ServletException;
-
-public class TransferAmountManager {
-
-	public boolean IsTransferSuccessfull(Connection connection,TransferAmountDetails transAmtDetails) throws ServletException {
-	
-		try{
-		AccountManager accountManager= new AccountManager();
-		TransactionManager transaction= new TransactionManager();
-		long senderAccountNo=transAmtDetails.getSenderAccountNo();
-		long receiverAccountNo=transAmtDetails.getReceiverAccountNo();
-		long amount=transAmtDetails.getAmount();
-		
-		Timestamp updatedTime= accountManager.getUpdateTime(connection, transAmtDetails.getSenderAccountNo());
-		if(updatedTime!=null && accountManager.IsAmountWithdrawn(connection,senderAccountNo,amount) && accountManager.IsAmountDeposited(connection,receiverAccountNo,amount)){
-			
-			if(transaction.insertRowForTransferAmount(connection,senderAccountNo,receiverAccountNo,amount)!=0)
-				return true;
-			else
-				return false;
-		}
-		}catch(Exception e){
-			System.out.println(e.getStackTrace());
-		}
-		return false;
-	}
-}
-=======
 package com.nrift.banking.utility;
 
 import java.sql.Connection;
@@ -76,4 +42,3 @@ public class TransferAmountManager {
 		return false;
 	}
 }
->>>>>>> a86bb14 transfer fund facility is partially done.

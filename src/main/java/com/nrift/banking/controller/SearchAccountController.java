31d21fa596985d96a729b61a75fdbb2f5f281692
Sneha-NRI <snehat@nrifintech.com>
2014-09-11 14:03:51 +0530

    added javadoc for classes related to search account

--- src/main/java/com/nrift/banking/controller/SearchAccountController.java
+++ src/main/java/com/nrift/banking/controller/SearchAccountController.java
@@ -21,57 +21,60 @@ import org.apache.log4j.Logger;
 import com.nrift.banking.dto.CustomerDTO;
 import com.nrift.banking.service.SearchAccountService;
 
+/**
+ * The Class SearchAccountController.
+ */
 @WebServlet(name = "SearchAccountController", urlPatterns = { "/searchAccountController" })
 public class SearchAccountController extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	static Logger logger = Logger.getLogger(SearchAccountController.class);
+    static Logger logger = Logger.getLogger(SearchAccountController.class);
 
-	/* (non-Javadoc)
-	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-	 */
-	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
-		Map<String, Object> reqParams = new HashMap<String, Object>();
-		if(request.getParameter("customerName")!="")
-			reqParams.put("customerName",request.getParameter("customerName"));
-		if(request.getParameter("customerId")!="")
-			reqParams.put("customerId",Long.parseLong(request.getParameter("customerId")));
-		if(request.getParameter("accountNo")!="")
-			reqParams.put("accountNo",Long.parseLong(request.getParameter("accountNo")));
-		if(request.getParameter("panNo")!="")
-			reqParams.put("panNo",request.getParameter("panNo"));
-		
-		
-		Connection con = (Connection) getServletContext().getAttribute("connection");
-		SearchAccountService searchAccountService = new SearchAccountService();
-		try{
-			List<CustomerDTO> customerList=searchAccountService.searchCustomerDetails(con,reqParams);
-			if (customerList != null) {
-				logger.info("Customer found with details=" + customerList);
-				HttpSession session = request.getSession();
-				session.setAttribute("customerList", customerList);				
-				response.sendRedirect("searchResult.jsp");
-			} else {
-				RequestDispatcher rd = getServletContext()
-						.getRequestDispatcher("/searchAccount.jsp");
-				PrintWriter out = response.getWriter();
-				logger.info("Customer Not found with details=");
-				out.println("<font color=red>No Customer found with given Details</font>");
-				rd.include(request, response);
-			}
-		}catch(SQLException |ServletException| IOException e) {
-			try {
+    /**
+     * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> reqParams = new HashMap<String, Object>();
+        if(request.getParameter("customerName")!="")
+            reqParams.put("customerName",request.getParameter("customerName"));
+        if(request.getParameter("customerId")!="")
+            reqParams.put("customerId",Long.parseLong(request.getParameter("customerId")));
+        if(request.getParameter("accountNo")!="")
+            reqParams.put("accountNo",Long.parseLong(request.getParameter("accountNo")));
+        if(request.getParameter("panNo")!="")
+            reqParams.put("panNo",request.getParameter("panNo"));
+
+
+        Connection con = (Connection) getServletContext().getAttribute("connection");
+        SearchAccountService searchAccountService = new SearchAccountService();
+        try{
+            List<CustomerDTO> customerList=searchAccountService.searchCustomerDetails(con,reqParams);
+            if (customerList != null) {
+                logger.info("Customer found with details=" + customerList);
+                HttpSession session = request.getSession();
+                session.setAttribute("customerList", customerList);				
+                response.sendRedirect("searchResult.jsp");
+            } else {
+                RequestDispatcher rd = getServletContext()
+                        .getRequestDispatcher("/searchAccount.jsp");
+                PrintWriter out = response.getWriter();
+                logger.info("Customer Not found with details=");
+                out.println("<font color=red>No Customer found with given Details</font>");
+                rd.include(request, response);
+            }
+        }catch(SQLException |ServletException| IOException e) {
+            try {
                 con.rollback();
             } catch(SQLException e1) {
                 logger.error("Rollback error");
             }
-			logger.error(" Exception Thrown");
-			//There should be an error block on around the top of every jsp page
-			request.setAttribute("errorMsg", "Exception Occured!");
+            logger.error(" Exception Thrown");
+            //There should be an error block on around the top of every jsp page
+            request.setAttribute("errorMsg", "Exception Occured!");
             request.getRequestDispatcher("login.html").forward(request,response);
-		}
-	}
+        }
+    }
 }
 
 
--- src/main/java/com/nrift/banking/service/SearchAccountService.java
+++ src/main/java/com/nrift/banking/service/SearchAccountService.java
@@ -7,29 +7,40 @@ import java.util.Map;
 
 import com.nrift.banking.dto.CustomerDTO;
 
+/**
+ * The Class SearchAccountService.
+ */
 public class SearchAccountService {
-	 
-	public List<CustomerDTO> searchCustomerDetails(Connection connection,Map<String, Object> reqParams) throws SQLException {
-		
-		String baseQuery="Select * from";
-		StringBuffer query= new StringBuffer(baseQuery);
-		query.append(" customer ")
-			 .append(" natural join ")
-			 .append(" account ")
-			 .append(" natural join ")
-			 .append(" account_type ");
-		if(!reqParams.isEmpty())
-			query.append(" where ");
-		if(reqParams.containsKey("customerName"))
-			query.append("name=?");
-		if(reqParams.containsKey("customerId"))
-			query.append("customer_id=?");
-		if(reqParams.containsKey("accountNo"))
-			query.append("account_number=?");
-		if(reqParams.containsKey("panNo"))
-			query.append("pan_number=?");
-		return new AdminService().getCustomerSearchDetails(connection, query.toString(), reqParams.values().toArray());
-			 
-	}	
+
+    /**
+     * Search customer details.
+     *
+     * @param connection the connection
+     * @param reqParams the req params
+     * @return the list
+     * @throws SQLException the SQL exception
+     */
+    public List<CustomerDTO> searchCustomerDetails(Connection connection,Map<String, Object> reqParams) throws SQLException {
+
+        String baseQuery="Select * from";
+        StringBuffer query= new StringBuffer(baseQuery);
+        query.append(" customer ")
+        .append(" natural join ")
+        .append(" account ")
+        .append(" natural join ")
+        .append(" account_type ");
+        if(!reqParams.isEmpty())
+            query.append(" where ");
+        if(reqParams.containsKey("customerName"))
+            query.append("name=?");
+        if(reqParams.containsKey("customerId"))
+            query.append("customer_id=?");
+        if(reqParams.containsKey("accountNo"))
+            query.append("account_number=?");
+        if(reqParams.containsKey("panNo"))
+            query.append("pan_number=?");
+        return new AdminService().getCustomerSearchDetails(connection, query.toString(), reqParams.values().toArray());
+
+    }	
 
 }

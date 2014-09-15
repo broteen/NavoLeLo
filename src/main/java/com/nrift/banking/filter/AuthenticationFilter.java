1412a5e05b716880443f2f3f6126a7d44df65719
Sneha-NRI <snehat@nrifintech.com>
2014-09-15 18:19:52 +0530

    changes in authentication filter

--- src/main/java/com/nrift/banking/filter/AuthenticationFilter.java
+++ src/main/java/com/nrift/banking/filter/AuthenticationFilter.java
@@ -18,6 +18,8 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
+import com.nrift.banking.validators.SessionValidator;
+
 
 /**
  * The Class AuthenticationFilter.
@@ -25,39 +27,47 @@ import org.apache.log4j.Logger;
 @WebFilter("/AuthenticationFilter")
 public class AuthenticationFilter implements Filter {
 
-    private Logger logger = Logger.getLogger(AuthenticationFilter.class);
+	private Logger logger = Logger.getLogger(AuthenticationFilter.class);
+	private FilterConfig filterConfig = null;
 
-    /**
-     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
-     */
-    public void init(FilterConfig fConfig) throws ServletException {
-        logger.info("AuthenticationFilter initialized");
-    }
+	/**
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+		logger.info("AuthenticationFilter initialized");
+	}
 
-    /**
-     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
-     */
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	/**
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
 
-        String uri = req.getRequestURI();
-        logger.info("Requested Resource::"+uri);
+		String uri = req.getRequestURI();
+		logger.info("Requested Resource::"+uri);
 
-        HttpSession session = req.getSession(false);
+		HttpSession session = req.getSession(false);
+		filterConfig.getInitParameter("exclude");
 
-        if(session == null && !(uri.endsWith("login.jsp") || uri.endsWith("register.jsp") || uri.endsWith("loginfo.jsp"))){
-            logger.error("Unauthorized access request");
-            res.sendRedirect("login.jsp");
-        }else{
-            // pass the request along the filter chain
-            chain.doFilter(request, response);
-        }
-    }
+		if(SessionValidator.validate(session, req.getRequestURI(), filterConfig.getInitParameter("exclude"))){
+			res.setHeader("Cache-Control", "no-cache");
+			res.setHeader("Cache-Control", "no-store");
+			res.setHeader("Cache-Control", "must-revalidate");
+			res.setHeader("Pragma", "no-cache");
+			res.setHeader("Expires","0");
+			chain.doFilter(request, response);
+		}else{
+			logger.error("Unauthorized access request");
+			res.sendRedirect("login.jsp");
+			
+		}
+	}
 
-    public void destroy() {
-        //close any resources here
-    }
+	public void destroy() {
+		//close any resources here
+	}
 
 }

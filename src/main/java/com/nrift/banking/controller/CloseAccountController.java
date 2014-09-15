8c809a246713a756226cf09c98b3d3b43c0e1d9a
arnaba <arnaba@ARNABA.user.nrifintech.com>
2014-09-15 14:33:05 +0530

    Account Delete is done

--- src/main/java/com/nrift/banking/controller/CloseAccountController.java
+++ src/main/java/com/nrift/banking/controller/CloseAccountController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
-
+import com.nrift.banking.service.CloseAccountService;
 import com.nrift.banking.dto.UserDTO;
 import com.nrift.banking.exception.BankingException;
 import com.nrift.banking.service.CloseAccountService;
